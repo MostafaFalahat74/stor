b7ca89bbcd08ec806ebf54050e949ebf78f99cd0
m4801@Mostafa <mostafa.falahat@gmail.com>
2023-09-22 11:58:44 -0700

    work on header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,18 @@
 const Header = () => {
   return (
     <div className="header">
-      <div className="headr-icons"></div>
+      <div className="headr-icons">
+        <span>2</span>
+        <span>2</span>
+        <span>2</span>
+        <span>2</span>
+      </div>
       <span>SHOPY</span>
-      <div className="headr-buttons"></div>
+      <div className="headr-buttons">
+       <button>SALE</button>
+       <button>DOCE</button>
+       <button>HOME</button>
+      </div>
     </div>
   );
 };
