3263e39e4ed5c64982c1b774665da3a6215982a7
m4801@Mostafa <mostafa.falahat@gmail.com>
2023-09-22 11:07:16 -0700

    work on header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-
+import Header from "./components/Header";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-یی
-      </header>
+    <div className="container">
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="headr-icons"></div>
+      <span>SHOPY</span>
+      <div className="headr-buttons"></div>
+    </div>
+  );
+};
+export default Header;
